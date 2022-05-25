56f08677d7c1d3faa0a92cc53647522a929a3d72
miguedeabreu <miguedeabreu@gmail.com>
2022-05-25 11:39:32 +0200

    Creacion de componentes, elementos y definicion de rutas

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const App = () => {
   return ( 
-    <div>Hola Mundo!</div>
+    <h1>Hola Mundo!</h1>
   );
 }
  
--- src/index.js
+++ src/index.js
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import WebFont from 'webfontloader';
+import Contenedor from './elementos/contenedor';
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import EditarGasto from './componentes/editarGasto';
+import GastosPorCategoria from './componentes/gastosPorCategoria';
+import InicioSesion from './componentes/inicioSesion';
+import ListaDeGastos from './componentes/listaDeGastos';
+import Registro from './componentes/registro';
 
 WebFont.load({
   google: {
@@ -13,6 +20,17 @@ WebFont.load({
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Contenedor>
+        <Routes>
+          <Route path='/inicio-sesion' element={<InicioSesion/>}/>
+          <Route path='/registro' element={<Registro/>}/>
+          <Route path='/categorias' element={<GastosPorCategoria/>}/>
+          <Route path='/lista' element={<ListaDeGastos/>}/>
+          <Route path='editar/:id' element={<EditarGasto/>}/>
+          <Route path='/' element={<App/>}/>
+        </Routes>
+      </Contenedor>
+    </BrowserRouter>
   </React.StrictMode>
 );
