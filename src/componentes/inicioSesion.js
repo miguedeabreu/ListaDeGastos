507f1c56a646454e768ea2aef1f244fd9f1044e9
miguedeabreu <miguedeabreu@gmail.com>
2022-05-26 16:04:46 +0200

    Sección de Registro e IniciarSesion maquetadas

--- src/componentes/inicioSesion.js
+++ src/componentes/inicioSesion.js
@@ -1,8 +1,51 @@
 import React from "react";
+import Helmet from "react-helmet";
+import {Header, Titulo, ContenedorHeader} from "../elementos/header";
+import Boton from "../elementos/boton";
+import {Formulario, Input, ContenedorBoton} from "../elementos/elementosDeFormulario";
+import {ReactComponent as SvgLogin} from "../multimedia/login.svg";
+import styled from "styled-components";
+
+const Svg = styled(SvgLogin)`
+    width: 100%;
+    max-height: 12.5rem; /* 200px */
+    margin-bottom: 1.25rem; /* 20px */
+`
 
 const InicioSesion = () => {
     return ( 
-        <h1>Inicio Sesion</h1>
+        <>
+            <Helmet>
+                <title>Iniciar Sesión</title>
+            </Helmet>
+
+            <Header>
+                <ContenedorHeader>
+                    <Titulo>Iniciar Sesión</Titulo>
+                    <div>
+                        <Boton to="/registro">Registrarse</Boton>
+                    </div>
+                </ContenedorHeader>
+            </Header>
+
+            <Formulario>
+                <Svg />
+                <Input
+                    type="email"
+                    name="email"
+                    placeholder="Correo Electrónico"
+                />
+                <Input
+                    type="password"
+                    name="password"
+                    placeholder="Contraseña"
+                />
+                <ContenedorBoton>
+                    <Boton as="button" primario type="submit">Iniciar Sesión</Boton>
+                </ContenedorBoton>
+    
+            </Formulario>
+        </>
     );
 }
  
--- src/componentes/registro.js
+++ src/componentes/registro.js
@@ -1,8 +1,56 @@
 import React from "react";
+import Helmet from "react-helmet";
+import {Header, Titulo, ContenedorHeader} from "../elementos/header";
+import Boton from "../elementos/boton";
+import {Formulario, Input, ContenedorBoton} from "../elementos/elementosDeFormulario";
+import {ReactComponent as SvgLogin} from "../multimedia/registro.svg";
+import styled from "styled-components";
+
+const Svg = styled(SvgLogin)`
+    width: 100%;
+    max-height: 6.25rem; /* 100px */
+    margin-bottom: 1.25rem; /* 20px */
+`
 
 const RegistroUsuarios = () => {
     return ( 
-        <h1>Registro de Usuarios</h1>
+        <>
+            <Helmet>
+                <title>Crear Cuenta</title>
+            </Helmet>
+
+            <Header>
+                <ContenedorHeader>
+                    <Titulo>Crear Cuenta</Titulo>
+                    <div>
+                        <Boton to="/inicio-sesion">Iniciar Sesión</Boton>
+                    </div>
+                </ContenedorHeader>
+            </Header>
+
+            <Formulario>
+                <Svg />
+                <Input
+                    type="email"
+                    name="email"
+                    placeholder="Correo Electrónico"
+                />
+                <Input
+                    type="password"
+                    name="password"
+                    placeholder="Contraseña"
+                />
+                <Input
+                    type="password"
+                    name="password2"
+                    placeholder="Repetir Contraseña"
+                />
+                <ContenedorBoton>
+                    <Boton as="button" primario type="submit">Crear Cuenta</Boton>
+                </ContenedorBoton>
+    
+            </Formulario>
+        </>
     );
 }
  
